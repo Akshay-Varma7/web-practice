f30d98a04588948be8d7ed965cc41c59b7546fd6
Akshay-Varma7 <jampaniakshay@gmail.com>
2025-09-27 23:43:20 +0530

    flash buttons & level up

--- simon-says/app.js
+++ simon-says/app.js
@@ -1,13 +1,37 @@
 let gameSeq = [];
 let userSeq = [];
 
+let btns = ["red","yellow","green","purple"];//class names
+
 let started = false;
 let level = 0;
 
+let i = document.querySelector("i");
 
 document.addEventListener("click",function (){//anywhere on webpage
     if(started == false){//to start only once + not "false"
         console.log("game is started");
         started = true ;
+
+        levelUp();
     }
 });
+
+function btnFlash(btn){
+    btn.classList.add("flash");//shld exist in css
+    setTimeout(function (){
+        btn.classList.remove("flash");
+    },250);
+}
+
+function levelUp(){
+    level++;
+    i.innerText =`level ${level}`
+
+    //random btn choose
+    let randomIdx = Math.floor(Math.random()*4);// 0 to 3
+    let randomColor = btns[randomIdx];
+    let randomBtn = document.querySelector(`.${randomColor}`);// . for class+ `` string 
+
+    btnFlash(randomBtn);
+}
