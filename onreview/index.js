076e74133541cf96626a8b7755b6dd159d096ade
Akshay-Varma7 <jampaniakshay@gmail.com>
2025-10-29 14:46:21 +0530

    new

---
+++ onreview/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const port =8080;
+const path = require("path");
+
+app.use(express.urlencoded({extended : true}));
+app.use(express.json());
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+
+app.set(express.static(path.join(__dirname,"public")));
+
+app.get("/",(req,res)=>{
+    res.send("server running well");
+});
+app.listen(port, () => {
+    console.log("listening to port 8080");
+});
