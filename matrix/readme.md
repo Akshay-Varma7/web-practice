d3a007e4eaf9d234f09f2b3d19c15c344462279d
Akshay-Varma7 <jampaniakshay@gmail.com>
2025-11-04 23:40:43 +0530

    progress

---
+++ matrix/readme.md
@@ -0,0 +1,1 @@
+an visual matrix elementary operations
--- onreview/index.js
+++ onreview/index.js
@@ -9,10 +9,35 @@ app.use(express.json());
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 
-app.set(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname,"public")));//mw used everytime req
 
-app.get("/",(req,res)=>{
-    res.send("server running well");
+let posts = [
+    {
+        username : "akshay",
+        content : "my first post"
+    },
+    {
+        username : "mahathi",
+        content : "i am engineer"
+    },
+    {
+        username : "dheeraj",
+        content : "i am 25"
+    }
+];
+//REST full apis
+//1.
+app.get("/posts",(req,res)=>{
+    res.render("index.ejs",{posts});//if we send something it shld be used
+});
+//2.
+app.get("/posts/new",(req,res)=>{
+    res.render("new.ejs");
+});
+app.post("/posts",(req,res)=>{//sends data in body//posts in /posts route where we see all posts
+    let {username ,content} = req.body;
+    posts.push({username ,content})//whenever in js {val1,val2} it creates object with key as val1 and val2 ie., {val1:val1,val2:val2}
+    res.redirect("/posts");//forces same page to make a get req to route or url or page mentioned
 });
 app.listen(port, () => {
     console.log("listening to port 8080");
